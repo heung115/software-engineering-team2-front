f5dc3c757f867e4250c11589244fe0a810172f2d
BieNew22 <newbie11004@gmail.com>
2024-04-25 04:08:35 +0900

    ✨ [TC-11] 검색 로그 보이기 추가함.
    - 서버로 부터 검색 로그를 가져와서 보여줌.

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,36 @@
+import ServerAPI from '../components/ServerAPI';
 import '../styles/MainPage.css';
-const { Component } = require('react');
+import { Component } from 'react';
 
 class MainPage extends Component {
+    state = {
+        haveLog: false,
+        focusOn: false,
+        searchLog: [],
+    };
+
+    focusOnSearch = async () => {
+        try {
+            if (!this.state.haveLog) {
+                const result = await ServerAPI.get(
+                    'http://localhost:8000/hello',
+                );
+
+                console.log(result.data['search']);
+                this.setState({
+                    haveLog: true,
+                    focusOn: true,
+                    searchLog: result.data['search'],
+                });
+            } else {
+                this.setState({ focusOn: true });
+            }
+        } catch (error) {
+            console.log(error);
+            this.setState({ focusOn: false, searchLog: [] });
+        }
+    };
+
     render() {
         return (
             <div className="main">
@@ -9,13 +38,30 @@ class MainPage extends Component {
 
                 <div className="container">
                     <div className="logo"></div>
-                    <fieldset className="field-container">
-                        <input
-                            className="field"
-                            type="text"
-                            placeholder="Search..."
-                        />
-                        <div className='icon-search'></div>
+                    <fieldset
+                        className={
+                            this.state.focusOn
+                                ? 'field-container expand_field_container'
+                                : 'field-container'
+                        }
+                    >
+                        <div className="field-area">
+                            <input
+                                className="field"
+                                type="text"
+                                placeholder="Search..."
+                                onFocus={this.focusOnSearch}
+                                onBlur={() => this.setState({ focusOn: false })}
+                            />
+                            <div className="icon-search"></div>
+                        </div>
+
+                        {this.state.searchLog.map((content, idx) => (
+                            <div className='search-log'>
+                                <div className='search-icon'></div>
+                                <div className="search-content">{content}</div>
+                            </div>
+                        ))}
                     </fieldset>
                 </div>
             </div>

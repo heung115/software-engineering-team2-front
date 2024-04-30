5d4fb4ddcd9d1b915938b43729e066175515cf43
heung115 <heung115@naver.com>
2024-04-30 10:24:16 +0900

    ✨ 모달 컴포넌트 추가 및 로그인 및 회원가입 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './pages/Login';
+import SignupForm from './pages/SignupForm';
+import Modal from './components/Modal';
 import UserProfile from './components/UserProfile';
-
+import './App.css';
 const App = () => {
+    const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+    const [isSignupModalOpen, setIsSignupModalOpen] = useState(false);
+
+    const openLoginModal = () => setIsLoginModalOpen(true);
+    const closeLoginModal = () => setIsLoginModalOpen(false);
+    const openSignupModal = () => setIsSignupModalOpen(true);
+    const closeSignupModal = () => setIsSignupModalOpen(false);
+
     return (
-        <div>
+        <div className="App">
             <h1>Supabase Auth Example</h1>
             <UserProfile />
-            <Login />
+            <br></br>
+            <div className="button-container">
+                <button onClick={openLoginModal}>Login</button>
+                <button onClick={openSignupModal}>Sign Up</button>
+            </div>
+            <Modal isOpen={isLoginModalOpen} onClose={closeLoginModal}>
+                <Login />
+            </Modal>
+            <Modal isOpen={isSignupModalOpen} onClose={closeSignupModal}>
+                <SignupForm closeModal={closeSignupModal} />
+            </Modal>
         </div>
     );
 };
